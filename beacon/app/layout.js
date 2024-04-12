5c9b8df86aa1ddea9e40cb5e95b26273ac93a0a8
Diptayan Jash <dj2037@srmist.edu.in>
2024-04-13 00:59:00 +0530

    Update favicon in layout.js and metadata

--- beacon/app/layout.js
+++ beacon/app/layout.js
@@ -9,8 +9,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Beacon - Simplifying Mass Mailing",
   description: "Beacon offers user-friendly, scalable mass mailing solutions designed to boost your marketing efforts and reach your audience effectively.",
-  image: "https://mailer-6i6.pages.dev/logo.png",
+  image: "../public/favicon.png",
   url: "https://beacon.djdiptayan.in/",
+  icons: {
+    icon: "../public/favicon.png",
+  },
 };
 
 
@@ -21,14 +24,14 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://mailer-6i6.pages.dev/logo.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="https://mailer-6i6.pages.dev/logo.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="https://mailer-6i6.pages.dev/logo.png" />
+        <link rel="icon" href="../public/favicon.png" sizes='any' />
+        <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="../public/favicon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="https://mailer-6i6.pages.dev/logo.png" />
+        <meta property="og:image" content="../public/favicon.png" />
         <meta property="og:url" content="https://beacon.djdiptayan.in/" />
         <meta property="og:site_name" content="Beacon" />
         <link rel="canonical" href="https://beacon.djdiptayan.in/" />
