ff50c654b4e925f98673ccecc75ba4ea70fbdc3e
Diptayan Jash <dj2037@srmist.edu.in>
2024-04-14 01:00:13 +0530

    Update hello route.js to include new API message

--- beacon/app/api/hello/route.js
+++ beacon/app/api/hello/route.js
@@ -1,5 +1,5 @@
 export async function GET(request) {
-    return new Response(JSON.stringify({ message: 'Hello, world!' }), {
+    return new Response(JSON.stringify({ message: 'Hello, world Testing API!' }), {
         status: 200,
         headers: {
             'Content-Type': 'application/json',
